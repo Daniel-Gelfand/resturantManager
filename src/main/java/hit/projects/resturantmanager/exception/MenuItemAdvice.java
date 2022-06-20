26dfe8b925dea8dda4b987d62895b722b10630c4
yarinbenbaruch <yarinbenbaruch97@gmail.com>
2022-06-20 21:30:28 +0300

    Change exception

--- src/main/java/hit/projects/resturantmanager/exception/MenuItemAdvice.java
+++ src/main/java/hit/projects/resturantmanager/exception/MenuItemAdvice.java
@@ -2,19 +2,27 @@ package hit.projects.resturantmanager.exception;
 
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @ControllerAdvice
 public class MenuItemAdvice {
 
-    @ResponseBody
-    @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(MenuItemException.class)
-    String menuItemNotFoundHandler(MenuItemException mie){
-        return mie.getMessage();
+    public ResponseEntity<Object> menuItemNotFoundHandler(MenuItemException mie){
+        Map<String, Object> body = new LinkedHashMap<>();
+
+        body.put("time", LocalDateTime.now());
+        body.put("message", mie.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
     }
 
     String menuItemPriceNotFoundHandler(MenuItemException mie) {
