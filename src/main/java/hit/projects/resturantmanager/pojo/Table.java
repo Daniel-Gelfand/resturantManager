5140693a99dd273436165102f34a1528bc3dc952
matan <matanbare25@gmail.com>
2022-06-28 20:56:13 +0300

    delete and crete method for table controller been added

--- src/main/java/hit/projects/resturantmanager/pojo/Table.java
+++ src/main/java/hit/projects/resturantmanager/pojo/Table.java
@@ -4,6 +4,7 @@ import hit.projects.resturantmanager.enums.TableStatus;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.ReadOnlyProperty;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.DocumentReference;
@@ -23,18 +24,20 @@ public class Table {
     @Id
     private String id;
     private TableStatus tableStatus;
+    @Indexed(unique = true)
     private int tableNumber;
 
     @DBRef
     private List<Order> orderList;
-
+    @DBRef
+    private List<Waiter> waitersList;
 
     @Builder
     public Table(TableStatus tableStatus, int tableNumber) {
         this.tableStatus = tableStatus;
         this.tableNumber = tableNumber;
         this.orderList = new ArrayList<>();
+        this.waitersList = new ArrayList<>();
     }
-
 
 }
